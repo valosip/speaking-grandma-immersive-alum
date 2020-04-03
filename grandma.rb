ec3c9f0d0472eaab7ae58c0fe76e9a47d9b0e799
Val Osip <valosip@gmail.com>
2020-04-03 20:09:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma(string)
 if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif string != string.upcase
-  return "HUH?! SPEAK UP, SONNY!"
+elsif string == string.upcase
+  "NO, NOT SINCE 1938!"
 else
-  return 
+  "HUH?! SPEAK UP, SONNY!"
 end
